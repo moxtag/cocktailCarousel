19e3758eda656a2f4fe06f5b91bc9633e2441390
moxtag <101368632+moxtag@users.noreply.github.com>
2026-02-28 00:09:44 -0800

    cleaned up, created todo for next session

--- js/main.js
+++ js/main.js
@@ -5,16 +5,22 @@ document.querySelector('button').addEventListener('click', getDrink)
 function getDrink () {
 
     let drink = formatDrink(document.querySelector('input').value)
-    console.log(drink)
 
     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drink)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-      console.log(data.drinks)
+
+      console.log(data)
+    
+      // TODO - CREATE CAROUSEL BROUGHT UP BY SEARCH
+
       let item = data.drinks[0]
       document.querySelector('h2').innerText = `\n${item.strDrink}`
       document.querySelector('img').src = `${item.strDrinkThumb}`
       document.querySelector('h3').innerText = `\n${item.strInstructions}`
+
+      // TO DO - INSERT INGREDIENTS AND INGREDIENTS AS WELL
+ 
     })
     .catch(err => {
         console.log(`error ${err}`)
