b0cd4d67cc44b16d3e8033109ddcf3e95f505d44
moxtag <101368632+moxtag@users.noreply.github.com>
2026-02-27 22:31:30 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+document.querySelector('button').addEventListener('click', getDrink)
+
+function getDrink () {
+    let drink = document.querySelector('input').value
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+    .then(res => res.json()) // parse response as JSON
+    .then(data => {
+      console.log(data.drinks[0])
+      let item = data.drinks[0]
+      document.querySelector('h2').innerText = `\n${item.strDrink}`
+      document.querySelector('img').src = `${item.strDrinkThumb}`
+      document.querySelector('h3').innerText = `\n${item.strInstructions}`
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+}
